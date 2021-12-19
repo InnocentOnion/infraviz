0bb5d1848253ea7578e8712fa7caa8420d056398
Curious-Cucumber <96306141+Curious-Cucumber@users.noreply.github.com>
2021-12-19 15:05:34 +0100

    Aktuelle Version Main Transformation

--- Main_transformation.R
+++ Main_transformation.R
@@ -3,11 +3,10 @@ library(sf)
 #Zuordnen von administrativen Bezikrne zu Infrastrukturpunkten
 
 #Landkreise laden
-landkreise3<-st_read(dsn = "/Users/georgwamsler/Dropbox (Privat)/Universität/9.Semester/Data Science II/All_data/DEU_adm", layer = "DEU_adm3")
-
+landkreise3<-st_read(dsn = "/Users/georgwamsler/Dropbox (Privat)/Universität/9.Semester/Data Science II/All_data/nuts5000_12-31.gk3.shape/nuts5000", layer = "5000_NUTS3")
+head(landkreise3)
 #Daten in Haupttabelle transformieren
 geodaten_gesamt<-infradata %>%
-
   filter(Latitude!="NA")
 geodaten_gesamt<-st_as_sf(geodaten_gesamt, coords = c("Longitude", "Latitude"))
 #Projektionen zuordnen
@@ -17,10 +16,8 @@ k_trans<-st_transform(landkreise3, 2163)
 k_trans
 
 k_trans<-k_trans %>%
-  select(NAME_3, ID_3, geometry)
+  select(NUTS_NAME,NUTS_CODE, geometry)
 apo_join<-st_join(pnts_trans_all, k_trans, join = st_within)
-apo_count<-count(as_tibble(apo_join), NAME_3) %>% 
-  arrange(-n) %>%
-  print()
+
 as_tibble(apo_join)
 write.csv(apo_join, "/Users/georgwamsler/Dropbox (Privat)/Universität/9.Semester/Data Science II/All_data/infradata_gesamt.csv")
