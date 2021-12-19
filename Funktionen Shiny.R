5d06edbee71762adede1bfe4dc1c5b587761dd72
InnocentOnion <76818567+InnocentOnion@users.noreply.github.com>
2021-12-19 14:41:54 +0100

    Update Funktionen Shiny.R

--- Funktionen Shiny.R
+++ Funktionen Shiny.R
@@ -4,8 +4,8 @@ library(sf)
 
 adresse_finden<-function(a){
   df<-data.frame(Adresse = (a))
-  Adresse<-geocode(df, method = "osm", lat = Latitude, long = Longitude)
-  return(Adresse)
+  a_conv<-df %>%
+    geocode(Adresse, method = "osm", lat = Latitude, long = Longitude)
+  return(a_conv)
 }
 G<-adresse_finden("Oberstraße 59, Witten")
-?as.data.frame
