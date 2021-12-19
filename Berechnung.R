66b8d8e4d81ef1c8512ef8acb1b68f7fcbe7bd73
InnocentOnion <76818567+InnocentOnion@users.noreply.github.com>
2021-12-19 12:57:22 +0100

    Update Berechnung.R

--- Berechnung.R
+++ Berechnung.R
@@ -10,15 +10,20 @@ geo_data_transformed<-geo_data_raw %>%
   count(type) 
 
 
-
+#Populationsdaten importieren
 pop_data<-read_excel("/Users/cedric/Dropbox/Data Science II/All_data/Bevölkerung_und_Fläche_Kreise.xlsx") 
 
-length(geo_data_transformed$NUTS_CODE)
 
+#Pop-Daten und Geodaten zusammenführen + Neue SPalten hinzufügen
 geo_data_full<-geo_data_transformed %>%
   inner_join(pop_data, by = c("NUTS_CODE"="NUTS3")) %>%
+  select(-Schlüsselnummer, -männlich, -weiblich) %>%
+  mutate(Infra_by_pop = insgesamt/n) %>%
+  mutate(Infra_by_fläche = Fläche/n) %>%
   group_by(type) %>%
-  mutate(max(n))
+  mutate(min(Infra_by_pop)) %>%
+  mutate(INfra_rank = (min(Infra_by_pop)/Infra_by_pop))
+  
 head(geo_data_full)
 
 
