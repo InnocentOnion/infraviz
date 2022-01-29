9b3e9fd36aa6ae630ecf14d2b1d8b08d6e21beb0
InnocentOnion <76818567+InnocentOnion@users.noreply.github.com>
2022-01-29 12:41:07 +0100

    Pfadnamen angepasst
    Pfad-Option hinzugefügt, um schnell zwischen unseren Dropboxen zu wechseln

--- Web.AppGeorg/global.r
+++ Web.AppGeorg/global.r
@@ -7,9 +7,13 @@ library(shinydashboard)
 library(broom)
 library(rgdal)
 
-geo_data_merged<-read_csv("/Users/georgwamsler/Dropbox (Privat)/Universität/9.Semester/Data Science II/All_data/geo_data_merged.csv")
-lk<-readOGR(dsn = "/Users/georgwamsler/Dropbox (Privat)/Universität/9.Semester/Data Science II/All_data/nuts5000_12-31.gk3.shape/nuts5000", layer = "5000_NUTS3")
-landkreise3<-st_read(dsn = "//Users/georgwamsler/Dropbox (Privat)/Universität/9.Semester/Data Science II/All_data/nuts5000_12-31.gk3.shape/nuts5000", layer = "5000_NUTS3")
+Cedric= "/Users/cedric/Dropbox/Data Science II/All_data"
+Georg = "/Users/georgwamsler/Dropbox (Privat)/Universität/9.Semester/Data Science II/All_data"
+pfad = Cedric
+
+geo_data_merged<-read_csv(paste(pfad,"geo_data_merged.csv", sep = "/"))
+lk<-readOGR(dsn = paste(pfad,"nuts5000_12-31.gk3.shape/nuts5000", sep = "/"), layer = "5000_NUTS3")
+landkreise3<-st_read(dsn = paste(pfad,"nuts5000_12-31.gk3.shape/nuts5000", sep = "/"), layer = "5000_NUTS3")
 k_trans <- st_transform(landkreise3, 2163) %>%
   select(NUTS_CODE, NUTS_NAME, geometry)
 
